e1a93c44ed42ed55b76675c6de2d8bf9e43d013a
jpaloalto <164952964+jpaloalto@users.noreply.github.com>
2024-05-07 15:03:05 -0700

    Create randomvalues.tf

---
+++ randomvalues.tf
@@ -0,0 +1,25 @@
+resource "random_integer" "priority31" {
+  min = 1
+  max = 50000
+}
+
+resource "random_integer" "priority32" {
+  min = 1
+  max = 50000
+}
+
+resource "random_integer" "priority33" {
+  min = 1
+  max = 50000
+}
+
+
+resource "random_integer" "priority34" {
+  min = 1
+  max = 50000
+}
+
+resource "random_integer" "priority35" {
+  min = 1
+  max = 50000
+}
